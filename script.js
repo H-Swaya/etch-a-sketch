a7c3f33ac51798086eb6e87ec77e1dd94ab7052b
Hans Perez <hanspz1011@gmail.com>
2026-08-02 02:30:54 +0300

    Add function for making 16x16 boxes

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // A for loop to make the boxes, it should scale to the size of the container
+let container = document.querySelector("#container");
+
+function makeBoxes() {
+  for (let i = 0; i < 256; i++) {
+    let boxesItem = document.createElement("div");
+    boxesItem.className = "boxes";
+    container.appendChild(boxesItem);
+  }
+}
+
+makeBoxes();
+
+// HTML node Selectors and decalrations
