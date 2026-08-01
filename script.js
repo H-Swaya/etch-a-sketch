fc0a35761219571d7d311fa12cec3a3b5534c728
Hans Perez <hanspz1011@gmail.com>
2026-08-02 02:49:56 +0300

    Add event listener for each box in container

--- script.js
+++ script.js
@@ -2,11 +2,22 @@
 let container = document.querySelector("#container");
 
 function makeBoxes() {
+  // BOX CREATING FUNCTION
   for (let i = 0; i < 256; i++) {
     let boxesItem = document.createElement("div");
     boxesItem.className = "boxes";
     container.appendChild(boxesItem);
   }
+
+  //   ADD EVENT LISTENERS FOR EACH BOX
+  let boxes = document.querySelectorAll(".boxes");
+  console.log(boxes);
+  boxes.forEach((box) => {
+    box.addEventListener("mouseenter", () => {
+      console.log(box);
+      box.style.backgroundColor = "blue";
+    });
+  });
 }
 
 makeBoxes();
