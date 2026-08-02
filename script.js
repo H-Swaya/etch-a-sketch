8b679213b2285f5ee17e7a68e55f412b7b2c01f6
Hans Perez <hanspz1011@gmail.com>
2026-08-02 04:16:50 +0300

    Tweak reset function & boxes width, height scaling

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 let container = document.querySelector("#container");
-
-function makeBoxes() {
+makeBoxes(16);
+function makeBoxes(userInput) {
   // IF CONTAINER IS NOT == 0 THEN EMPTY THE CONTAINER
   if (container.childElementCount !== 0) {
     container.replaceChildren();
   }
 
   // LOGIC FOR MAKING BOXES
-  console.log(container.childElementCount !== 0);
-  for (let i = 0; i < 256; i++) {
+
+  for (let i = 0; i < userInput * userInput; i++) {
     let boxesItem = document.createElement("div");
+    boxesItem.style.width = `${800 / userInput}px`;
+    boxesItem.style.height = `${800 / userInput}px`;
     boxesItem.className = "boxes";
     container.appendChild(boxesItem);
   }
 
   //   ADD EVENT LISTENERS FOR EACH BOX
   let boxes = document.querySelectorAll(".boxes");
-  console.log(boxes);
   boxes.forEach((box) => {
     box.addEventListener("mouseenter", () => {
-      console.log(box);
       box.style.backgroundColor = "blue";
     });
   });
@@ -27,9 +27,19 @@ function makeBoxes() {
 
 // BUTTON CALL TO RESET THE BOX
 const resetButton = document.querySelector("#resetButton");
-
+let previousUserInput = 16;
 resetButton.addEventListener("click", () => {
   const input = document.querySelector("#input");
-  makeBoxes(input.value);
+  if (input.value <= 100 && input.value != 0) {
+    makeBoxes(input.value);
+    previousUserInput = input.value;
+  } else if (input.value == 0) {
+    console.log(previousUserInput);
+    makeBoxes(previousUserInput);
+  } else {
+    alert(
+      "That size is either too big or not a number, please enter a number from 1-100",
+    );
+  }
   input.value = "";
 });
